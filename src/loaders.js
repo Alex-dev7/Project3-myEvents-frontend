47ebb8b1647f19215d5f8ced192c260957810429
kayfernander2022 <kayfernander@hotmail.com>
2023-01-22 17:10:15 -0500

    change loader link

--- src/loaders.js
+++ src/loaders.js
@@ -1,4 +1,5 @@
-const URL = "http://localhost:4000"
+// this URL is the dev branch
+const URL = "https://myevents-backend-dev.onrender.com"
 
 // loader for index route
 export const eventsLoader = async () => {
