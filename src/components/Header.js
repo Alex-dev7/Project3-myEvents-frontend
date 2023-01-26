e49726a81a72d14bf39c7f538f11b2df6e64dd57
Cindy Wong <cindywongdev@gmail.com>
2023-01-25 20:52:56 -0500

    started styling nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,19 +3,37 @@ import { Link } from 'react-router-dom'
 function Header(props) {
 
     return (
-        <header >
-            <div className='nav-bar'>
-                <Link to='/about'>
-                    About
-                </Link>
-                <Link to='/create'>
-                    Add New Event
-                </Link>
+        <header>
+            <div className="nav-container">
+                <nav className='navbar navbar-expand-lg navbar-dark'>
+                    <Link class="navbar-brand" to='/'>MyEvents</Link>
+                    <Link to='/about'>
+                        About
+                    </Link>
+                    <Link to='/create'>
+                        Add New Event
+                     </Link>
+                </nav>
             </div>
-           <Link id='title' to='/'>MyEvents</Link>
 
         </header>
     )
 }
 
 export default Header
+
+
+        // <header>
+        //     <div className="nav-container">
+        //         <nav className='nav-bar'>
+        //             <Link to='/about'>
+        //                 About
+        //             </Link>
+        //             <Link to='/create'>
+        //                 Add New Event
+        //             </Link>
+        //         </nav>
+        //         <Link id='title' to='/'>MyEvents</Link>
+        //     </div>
+
+        // </header>
