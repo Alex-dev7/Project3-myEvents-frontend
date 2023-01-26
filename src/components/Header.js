0e79b91a25ccfd35f002921b1991d523dfd3c817
Cindy Wong <cindywongdev@gmail.com>
2023-01-25 22:23:18 -0500

    basic dropdown nav completed for web and mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,38 @@ function Header(props) {
         <header>
             <div className="nav-container">
                 <nav className='navbar navbar-expand-lg navbar-dark'>
-                    <Link class="navbar-brand" to='/'>MyEvents</Link>
-                    <Link to='/about'>
-                        About
-                    </Link>
-                    <Link to='/create'>
-                        Add New Event
-                     </Link>
+                    <div className="container-fluid">
+                        <Link className="navbar-brand" to='/'>MyEvents</Link>
+                        <button
+                            className="navbar-toggler"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            // should be the id value of the content
+                            data-bs-target="#navbarSupportedContent"
+                            aria-controls="navbarSupportedContent"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                        >
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav ms-auto text-center">
+                                <li>
+                                    <Link to="/" className="nav-link">Home</Link>
+                                </li>
+                                <li>
+                                    <Link to="/create" className="nav-link text-nowrap">Add New Event</Link>
+                                </li>
+                                <li>
+                                    <Link to="/about" className="nav-link">About</Link>
+                                </li>
+                                <li>
+                                    <Link to="#" className="nav-link">Logout</Link>
+                                </li>
+                            </ul>
+                        </div>
+
+                    </div>
                 </nav>
             </div>
 
@@ -21,19 +46,3 @@ function Header(props) {
 }
 
 export default Header
-
-
-        // <header>
-        //     <div className="nav-container">
-        //         <nav className='nav-bar'>
-        //             <Link to='/about'>
-        //                 About
-        //             </Link>
-        //             <Link to='/create'>
-        //                 Add New Event
-        //             </Link>
-        //         </nav>
-        //         <Link id='title' to='/'>MyEvents</Link>
-        //     </div>
-
-        // </header>
